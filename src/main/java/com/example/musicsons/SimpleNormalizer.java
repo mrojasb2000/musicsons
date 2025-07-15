e229f3ab1ec3eeea61d3dfee574b0085b1207813
Mauricio Rojas <mrojasb2000@gmail.com>
2025-07-15 10:44:28 -0400

    chore: add two implements for Normalizer interfaces

--- src/main/java/com/example/musicsons/SimpleNormalizer.java
+++ src/main/java/com/example/musicsons/SimpleNormalizer.java
@@ -2,7 +2,7 @@ package com.example.musicsons;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("foo")
 public class SimpleNormalizer implements Normalizer {
     /* inherits default transform() method from interface */
 }
